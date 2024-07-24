08464950b838f70d9c7edd1cd010d0f27866664c
Debjeet Chakraborty <debjeet.chakraborty.2001@gmail.com>
2024-07-24 16:37:45 +0530

    JS

---
+++ firstfile.js
@@ -0,0 +1,20 @@
+console.log(10+40);
+console.log("Some message");
+
+//Declaring and initializing
+let x = 10 , y = 20, z = 30;
+console.log(x,y,z);
+
+//Declaring and initializing
+let a = 2;
+console.log(a);
+a = "Web-development";
+console.log(a);
+
+//number
+let num = 100;
+console.log(typeof num);
+
+//string
+const firstName = "Debjeet";
+console.log(typeof firstName);
